908a3463a3594997520c63cafa28257de0a0e917
harshita <harshita1225@gmail.com>
2022-06-29 12:59:35 +0200

    task completed

--- solution.js
+++ solution.js
@@ -1,1 +1,24 @@
+let str ='Computer';
+console.log(str);
 
+console.log(str.length);
+console.log('the first alphabet of Computer is',str[0]);
+console.log('the sixth alphabet of computer is',str[5]);
+console.log('the last alphabet of computer is',str[str.length-1]);
+
+
+let firstName = 'abc';
+let age = "15";
+let year = "2022";
+console.log(firstName,'is',age,'in',year);
+
+let isMarried = "false";
+console.log(firstName,'is married:',isMarried);
+
+let programmingLanguage ="JavaScript";
+let isFun ="true";
+console.log(programmingLanguage,'is fun:',isFun);
+
+console.log('length of the string javascript is:',programmingLanguage.length);
+console.log('the first alphabet of javascript is',programmingLanguage[0]);
+console.log('the last alphabet of javascript is',programmingLanguage[9]);
