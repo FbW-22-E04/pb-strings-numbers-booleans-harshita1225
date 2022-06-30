0554661c89bb62edcd67e83ec3e31c565e80dab3
harshita <harshita1225@gmail.com>
2022-06-30 08:56:21 +0200

    edited

--- solution.js
+++ solution.js
@@ -1,24 +1,31 @@
-let str ='Computer';
+const str ='Computer';
 console.log(str);
 
-console.log(str.length);
-console.log('the first alphabet of Computer is',str[0]);
-console.log('the sixth alphabet of computer is',str[5]);
-console.log('the last alphabet of computer is',str[str.length-1]);
+console.log(str[1] , str[5]);
+
+//console.log(str.length);
+//console.log('the first alphabet of Computer is',str[0]);
+//console.log('the sixth alphabet of computer is',str[5]);
+//console.log('the last alphabet of computer is',str[str.length-1]);
 
 
-let firstName = 'abc';
-let age = "15";
-let year = "2022";
-console.log(firstName,'is',age,'in',year);
+const firstName = 'jack';
+const age = "15";
+const year = "2022";
 
-let isMarried = "false";
-console.log(firstName,'is married:',isMarried);
 
-let programmingLanguage ="JavaScript";
-let isFun ="true";
-console.log(programmingLanguage,'is fun:',isFun);
+console.log(firstName + " is" +  age + " in"  + year);
+
+const isMarried = "false";
+console.log(firstName + "is married:" + isMarried);
+
+const programmingLanguage = "JavaScript";
+const isFun = true;
+
+
+console.log(programmingLanguage + "is fun:" + isFun);
+
+
+console.log("programmingLanguage",programmingLanguage[0] , programmingLanguage[4]);
 
 console.log('length of the string javascript is:',programmingLanguage.length);
-console.log('the first alphabet of javascript is',programmingLanguage[0]);
-console.log('the last alphabet of javascript is',programmingLanguage[9]);
